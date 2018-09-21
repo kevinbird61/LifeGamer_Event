78f67422fdbf866aac2d0c38f6469e1254118141
kevinbird61 <kevinbird61@gmail.com>
2018-09-21 09:07:14 +0800

    Add structure prototype

---
+++ src/vendor/README.md
@@ -0,0 +1,8 @@
+# Reference 
+
+## Document reading
+* [go-yaml/yaml](https://github.com/go-yaml/yaml)
+* [JSON and Go](https://github.com/go-yaml/yaml)
+
+## Question 
+* [How to import local package](https://stackoverflow.com/questions/35480623/how-to-import-local-packages-in-go/35511866)
---
+++ src/vendor/parser/parser.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"fmt"
+	"encoding/json"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Structure definition
+
+// Provide function usage - load from file and read json
+func readJSON(url string) {
+
+}
+
+// Provide function usage - load from file and read yaml
+func readYAML(url string) {
+	
+}
