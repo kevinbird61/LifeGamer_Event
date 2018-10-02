ed7515556c0196f82a842482f48b5636ecec39c8
kevinbird61 <kevinbird61@gmail.com>
2018-10-02 16:33:53 +0800

    Add read function for Parser

--- cmd/main.go
+++ cmd/main.go
@@ -2,4 +2,16 @@ package main
 
 import (
 	"fmt"
+
+	"../internal/parser"
 )
+
+func main(){
+	// create parser
+	p := parser.Parser{}
+	p.ReadJSON("../test/test.json")
+
+	fmt.Println(p.Obj)
+
+	// get something useful from source file
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,20 +1,57 @@
 package parser
 
 import (
-	"fmt"
-	"encoding/json"
+	_ "bufio"
+	_ "fmt"
+	_ "io"
+	"io/ioutil"
+	_ "os"
 
+	"encoding/json"
 	"gopkg.in/yaml.v2"
 )
 
 // Structure definition
+type Object struct {
+	Name string	`json: "name"`
+	Model string `json: "data"`
+}
+
+type Event struct {
+	Event []Object
+}
+
+// Class: Parser
+type Parser struct {
+	Raw string
+	Obj []Object
+}
+
+// =============================================== Member function of Parser ===============================================
 
 // Provide function usage - load from file and read json
-func readJSON(url string) {
+func (p *Parser) ReadJSON(url string) {
+	// read file into string
+	data, err := ioutil.ReadFile(url)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Print(string(data))
+	p.Raw = string(data)
+	json.Unmarshal(data, &p.Obj)
 
+	// fmt.Println(p.obj[0])
 }
 
 // Provide function usage - load from file and read yaml
-func readYAML(url string) {
+func (p *Parser) readYAML(url string) {
+	// read file into string
+	data, err := ioutil.ReadFile(url)
+	if err != nil {
+		panic(err)
+	}
 	
+	p.Raw = string(data)
+	yaml.Unmarshal(data, &p.Obj)
+	// fmt.Println(p.obj[0])
 }
